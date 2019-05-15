815f19a84d2989a5faf1b317427531ecd75bf496
alekseik1 <1alekseik1@gmail.com>
2019-05-15 19:31:09 +0000

    fix: config better via `set()`

--- generators/config.py
+++ generators/config.py
@@ -6,4 +6,4 @@ class MainConfig:
     DB_NAME = 'flower_shop'
     N_PERSONS = 150
     MERCHANT_IDS = list(range(2, 10))
-    USER_IDS = list(range(10, N_PERSONS))
+    USER_IDS = sorted(list(set(range(N_PERSONS)) - set(MERCHANT_IDS)))
