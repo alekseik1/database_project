7c30502497e5a8e7787a77a265dbb9b7f36c6d18
alekseik1 <1alekseik1@gmail.com>
2019-05-15 17:14:58 +0000

    remove: fill_flowers.sql

--- fill_flowers.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO Flower (name, color, current_price, available) VALUES ('Гвоздика', 'RED', 40, 200);
-INSERT INTO Flower (name, color, current_price, available) VALUES ('Роза', 'RED', 60, 300);
-INSERT INTO Flower (name, color, current_price, available) VALUES ('Тюльпан', 'YELLOW', 40, 130);
-INSERT INTO Flower (name, color, current_price, available) VALUES ('Тюльпан', 'RED', 40, 50);
-INSERT INTO Flower (name, color, current_price, available) VALUES ('Орхидея', 'WHITE', 600, 20);
-INSERT INTO Flower (name, color, current_price, available) VALUES ('Орхидея', 'RED', 800, 7);
-INSERT INTO Flower (name, color, current_price, available) VALUES ('Нарцисс', 'YELLOW', 70, 20);
-INSERT INTO Flower (name, color, current_price, available) VALUES ('Нарцисс', 'BLUE', 80, 40);
