444e41970c0c761746bc2d6f620ffefcf6739875
alekseik1 <1alekseik1@gmail.com>
2019-05-15 18:46:44 +0000

    TEST: for transfer only

--- generators/config.py
+++ generators/config.py
@@ -5,4 +5,6 @@ class MainConfig:
     DB_PASSWORD = 'toor'
     DB_NAME = 'flower_shop'
     N_PERSONS = 150
+    MERCHANT_IDS = list(range(2, 10))
+    USER_IDS = list(range(10, N_PERSONS))
 
--- generators/fill_orders.py
+++ generators/fill_orders.py
@@ -7,11 +7,17 @@ from mimesis import Datetime
 db = MySQLdb.connect(host=config.DB_HOST, user=config.DB_USER, passwd=config.DB_PASSWORD, db=config.DB_NAME)
 c = db.cursor()
 
-for i in range(2, int(config.N_PERSONS/10)):
+for i in range(60):
     c.execute("""
         INSERT INTO `Order` (merchant_id, completed_at, user_id)
         VALUES (%s, %s, %s);
-    """, [i, Datetime().datetime(), r.randint(int(config.N_PERSONS/10) + 2, config.N_PERSONS)])
+    """, [
+        r.choice(config.MERCHANT_IDS),
+        Datetime().datetime(),
+        #r.choice(config.USER_IDS)
+        20
+        ]
+    )
 db.commit()
 
 c.close()
