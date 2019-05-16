d338c2b86f92c7dc4f072661eaa749cde6c1f602
alekseik1 <1alekseik1@gmail.com>
2019-05-16 10:34:04 +0000

    Revert "Revert "fix: more orders""
    This reverts commit 572a3c6dd360efbd0c3ed3782cee0f059622f7bc.

--- generators/config.py
+++ generators/config.py
@@ -5,6 +5,6 @@ class MainConfig:
     DB_PASSWORD = 'toor'
     DB_NAME = 'flower_shop'
     N_PERSONS = 150
-    N_ORDERS = 60
+    N_ORDERS = 5325
     MERCHANT_IDS = list(range(2, 10))
     USER_IDS = sorted(list(set(range(1, int(N_PERSONS/2))) - set(MERCHANT_IDS)))
